e49ebd350ff905cbee6aec44ccbd2196484ae1ca
Vitor Lany <vitorlany99@gmail.com>
2024-08-23 23:18:27 -0300

    feat: Add repo_info.csv file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from dotenv import load_dotenv
 import os
 from datetime import datetime
@@ -89,38 +90,37 @@ def calculate_days(date):
 
 # Função para coletar e imprimir informações dos repositórios
 def collect_and_print_repo_info(repos):
+    header = ["repo_name", "created_at", "age", "pull_requests", "releases", "updated_at", "language", "open_issues", "closed_issues", "closed_issues_percent"]
+    rows = []
+
     for repo in repos:
         owner = repo["owner"]["login"]
         repo_name = repo["name"]
 
         created_at = repo["created_at"]
-        pull_requests = get_pull_requests(owner, repo_name)
+        age = calculate_age(created_at)
+        pull_requests = get_pull_requests(owner, repo_name) # Procurar otimizar, consome muito
         releases = get_releases(owner, repo_name)
         updated_at = repo["updated_at"]
         language = repo["language"]
         open_issues = repo["open_issues"]
-        closed_issues = get_closed_issues(owner, repo_name)
+        closed_issues = get_closed_issues(owner, repo_name) # Procurar otimizar, consome muito
+
         closed_issues_percent = 0
         if open_issues > 0:
             closed_issues_percent = closed_issues/open_issues
 
-        print(f"Repository: {repo_name}")
-        print(f'Created at: {created_at}')
-        print(f'Age: {calculate_age(created_at)} years')
-        print(f'Pull requests: {pull_requests}')
-        print(f'Releases: {releases}')
-        print(f'Updated at: {updated_at}')
-        print(f'Days since updated at: {calculate_days(updated_at)}')
-        print(f'Language: {language}')
-        print(f'Open issues: {open_issues}')
-        print(f'Closed issues: {closed_issues}')
-        print('Percentage of closed issues: %.2f' % closed_issues_percent)
-        print("-" * 200)
+        rows.append([repo_name, created_at, age, pull_requests, releases, updated_at, language, open_issues, closed_issues, closed_issues_percent])
+        
+    with open('repo_info.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(rows)
 
 # Main
 if __name__ == "__main__":
     query = "stars:>0"
-    num_repos = 100 # Número de repositórios a serem coletados
+    num_repos = 2 # Número de repositórios a serem coletados
     try:
         popular_repos = get_popular_repos(query, num_repos)
         collect_and_print_repo_info(popular_repos)
